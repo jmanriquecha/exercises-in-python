3c6084cbc3dbd8088275fc1a67d77e87ef308506
Jorge Manrique <georgemanc.28@gmail.com>
2024-08-16 16:43:44 +0000

    Validación de campos de texto

---
+++ ejercicios/ciclos/valida_campo.py
@@ -0,0 +1,8 @@
+# Validar campo de formulario
+
+nombre_usuario = None
+
+while not nombre_usuario:
+    nombre_usuario = input('Por favor ingrese su nombre: ')
+
+print(f'Hola {nombre_usuario} bienvenido de nuevo. ')
